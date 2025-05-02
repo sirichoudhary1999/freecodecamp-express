0de4f6e481eb7021be5b6c72031bb2a0ea2b64f8
sirishal1999@gmail.com <sirisha.lekkala@unisys.com>
2025-05-02 19:26:38 +0530

    middleware

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ let app = express();
 
 console.log("Hello World")
 
+let filePath = __dirname + '/public'
+
+app.use(filePath, express.static(__dirname + '/public/style.css '));
+
 app.get('/', (req, res) => {
   let absolutePath = __dirname + '/views/index.html';
   res.sendFile(absolutePath);
